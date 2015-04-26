201dc7b7dd26023a90b403b43bccff27ab6e2b50
Robert Taraya <robert.taraya@gmail.com>
2015-04-26 12:42:37 -0500

    render tasks on project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,8 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @project = Project.find(params[:id])
+    @tasks = @project.tasks
   end
 
   # GET /projects/new
